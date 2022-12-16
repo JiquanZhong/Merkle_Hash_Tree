ea841274a0256ec110e92b901372f8812ff9e870
JiquanZhong <74232427+JiquanZhong@users.noreply.github.com>
2022-12-16 23:19:09 +0100

    Done

---
+++ src/Hash.java
@@ -0,0 +1,88 @@
+import java.security.MessageDigest;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class Hash {
+    public static void main(String[] args) {
+        String s1 = "1";
+        String s2 = "1";
+        String s3 = "2";
+
+        Hash h1 = new Hash(s1);
+        Hash h2 = new Hash(s2);
+        Hash h3 = new Hash(s3);
+
+        System.out.println("Hash1 has size " + h1.hashLength + ": " + h1.toString());
+        System.out.println("Hash2 has size " + h2.hashLength + ": " + h2.toString());
+        System.out.println("Hash3 has size " + h3.hashLength + ": " + h3.toString());
+    }
+
+    byte[] hash;
+    MessageDigest digest;
+    int hashLength;
+
+    public Hash() {
+        try {
+            digest = MessageDigest.getInstance("SHA256");
+            hashLength = digest.getDigestLength();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("No such algorithm");
+            System.exit(1);
+        }
+        hash = new byte[hashLength];
+    }
+
+    public Hash(String s) {
+        this();
+        try {
+            byte[] b = s.getBytes("UTF-8");
+            int bLength = b.length;
+            byte leaf[] = new byte[bLength + 1];
+            System.arraycopy(b, 0, leaf, 1, bLength);
+            leaf[0] = 0x00;
+            hash = digest.digest(leaf);
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("No such encoding type");
+            System.exit(1);
+        }
+    }
+
+    public Hash(Hash old) {
+        this();
+        assert (hashLength == old.hashLength);
+        for (int i = 0; i < old.hash.length; i++) hash[i] = old.hash[i];
+    }
+
+    public Hash(Hash h1, Hash h2) {
+        this();
+        assert (h1.hashLength == h2.hashLength);
+        assert (hashLength == h1.hashLength);
+        byte[] merge = new byte[1 + 2 * hashLength];
+        merge[0] = 0x01;
+        System.arraycopy(h1.hash, 0, merge, 1, hashLength);
+        System.arraycopy(h2.hash, 0, merge, 1 + hashLength, hashLength);
+        hash = digest.digest(merge);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(hash);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (!Hash.class.isAssignableFrom(obj.getClass()))
+            return false;
+        final Hash other = (Hash) obj;
+
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] != other.hash[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/MerkleTree.java
@@ -0,0 +1,84 @@
+public class MerkleTree{
+  Hash hash;
+  MerkleTree left, right;
+  int start, end, nextPower, size;
+
+
+  public MerkleTree(MerkleTree old) {
+    hash = new Hash(old.hash);
+    left = new MerkleTree(old.left);
+    right = new MerkleTree(old.right);
+    start = old.start;
+    end = old.end;
+    nextPower = old.nextPower;
+    size = old.size;
+  }
+
+  public MerkleTree(String s, int index) {
+    left = null;
+    right = null;
+    start = index;
+    end = index;
+    size = 1;
+    nextPower = 1;
+    hash = new Hash(s);
+  }
+
+  public MerkleTree(MerkleTree l, MerkleTree r) {
+    if (l.end != r.start - 1) {
+      System.out.println("Trees not contiguous, left end at " + l.end + "; right starts at " + r.start );
+      System.exit(1);
+    } else {
+      left = l;
+      right = r;
+      start = l.start;
+      end = r.end;
+      size = l.size + r.size;
+      if(size < Math.max(l.nextPower, r.nextPower)) nextPower = Math.max(l.nextPower, r.nextPower);
+      else nextPower = 2 * Math.max(l.nextPower, r.nextPower);
+      hash = new Hash(l.hash, r.hash);
+    }
+  }
+
+  public void display() {
+    System.out.println("Size of tree: " + size);
+    display_offset(0);
+  }
+
+  void display_offset(int offset) {
+    for (int i = 0; i < offset; i ++) {
+      System.out.print("  ");
+    }
+    System.out.println("["+ start + "," + end + "]: " + hash.toString());
+    if (left != null) left.display_offset(offset+1);
+    if (right != null) right.display_offset(offset+1);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null)
+      return false;
+    if (!MerkleTree.class.isAssignableFrom(obj.getClass()))
+      return false;
+    final MerkleTree other = (MerkleTree) obj;
+
+    if (start != other.start || end != other.end || nextPower != other.nextPower || size != other.size) {
+      System.out.println("Merkle trees differ at node: "+ "["+ start + "," + end + "] :");
+      return false;
+    }
+    if (!hash.equals(other.hash)) {
+        System.out.println("Merkle trees differ by hash at node: "+ "["+ start + "," + end + "]");
+        return false;
+    }
+    if (left == null || right == null) {
+      assert(right == null && left ==null);
+      if (other.left == null || other.right == null) {
+        assert(other.right == null && other.left == null);
+        return true;
+      }
+      System.out.println("Merkle trees differ at node: "+ "["+ start + "," + end + "] :");
+      return false;
+    }
+    return (left.equals(other.left) && (right.equals(other.right)));
+  }
+}
